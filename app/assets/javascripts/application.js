896b9ed0cb76a4e06a08b4f5388bc753f3efc70c
chrishewison1983 <ods.chrishewison@gmail.com>
2019-03-29 15:46:01 +0000

    Updates to files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,98 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Added javascripts
+$(document).ready(function () {
+
+     // $(".delete-training-provider").on("click", function (e) {
+     //      e.preventDefault();
+     //      $(this).closest('li').remove();
+     // });
+     //
+     // $(".delete-apprenticeship").on("click", function (e) {
+     //      e.preventDefault();
+     //      $(this).closest('li').remove();
+     // });
+
+     $('input[name="compare-app"]').on('change',function(e) {
+          // var checkedApprenticeships = $(this).is(':checked');
+          // var itemApprenticeshipTitle = $(this).closest('h2.heading-l').find('a.apprenticeship-title').text();
+
+          // if (checkedApprenticeships) {
+          //      // $('#compare-message-panel .comparison-item-title').append('<span>' + itemApprenticeshipTitle +'</span>');
+          // }
+          if (countCheckedApprenticeship() === 0) {
+               $('.training-provider-content').css('opacity', '1');
+               $('input[name="compare-provider"]').removeAttr("disabled");
+               $('a.delete-training-provider').attr('href', '#');
+               $('#compare-container').hide();
+               $('#compare-container #view-compare').hide();
+               // $('#compare-message-panel .comparison-item-title').remove('<span>' + itemApprenticeshipTitle +'</span>');
+          } else if (countCheckedApprenticeship() === 1) {
+               $('.training-provider-content').css('opacity', '0.2');
+               $('input[name="compare-provider"]').attr("disabled", true);
+               $('a.delete-training-provider').removeAttr('href');
+               $('#compare-container').show();
+               $('#compare-container #view-compare').hide();
+               $('#compare-container .comparison-type').text('apprenticeship');
+          } else if (countCheckedApprenticeship() >= 2) {
+               $('.training-provider-content').css('opacity', '0.2');
+               $('input[name="compare-provider"]').attr("disabled", true);
+               $('a.delete-training-provider').removeAttr('href');
+               $('#compare-container').show();
+               $('#compare-container #view-compare').show();
+               $('#compare-container .comparison-type').text('apprenticeships');
+          }
+
+          $('#compare-container .compare-number').text(countCheckedApprenticeship());
+
+          function countCheckedApprenticeship() {
+               return $('input[name="compare-app"]:checked').length;
+          }
+
+     });
+
+     $('input[name="compare-provider"]').on('change',function(e) {
+          // var checkedTrainingProvider = $(this).is(':checked');
+
+          if (countCheckedTrainingProvider() === 0) {
+               $('input[name="compare-app"]').removeAttr("disabled");
+               $('input[name="compare-app"]').parent().parent().css('opacity', '1');
+               $('#compare-container').hide();
+               $('#compare-container #view-compare').hide();
+          } else if (countCheckedTrainingProvider() === 1) {
+               $('input[name="compare-app"]').attr("disabled", true);
+               $('input[name="compare-app"]').parent().parent().css('opacity', '0.1');
+               $('#compare-container').show();
+               $('#compare-container #view-compare').hide();
+               $('#compare-container .comparison-type').text('training provider');
+          } else if (countCheckedTrainingProvider() >= 2) {
+               $('input[name="compare-app"]').attr("disabled", true);
+               $('input[name="compare-app"]').parent().parent().css('opacity', '0.1');
+               $('#compare-container').show();
+               $('#compare-container #view-compare').show();
+               $('#compare-container .comparison-type').text('training providers');
+          }
+
+          $('#compare-container .compare-number').text(countCheckedTrainingProvider());
+
+          function countCheckedTrainingProvider() {
+               return $("input[name='compare-provider']:checked").length;
+          }
+
+     });
+
+
+     ///// DELETE - PROVIDER
+     $('input[name="confirm-delete-provider"]').on('change',function(e) {
+          $('#delete-provider').removeClass('govuk-button--disabled');
+     });
+
+     ///// DELETE - PROVIDER
+     $('input[name="confirm-delete-apprenticeship"]').on('change',function(e) {
+          $('#delete-apprenticeship').removeClass('govuk-button--disabled');
+     });
+
+
+});
